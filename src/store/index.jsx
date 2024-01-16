c11e0049fb17cf9f722c9939302a428eb078e8d5
WesleyBispo <wesley17bispo@gmail.com>
2024-01-15 22:28:37 -0300

    Ajustando config do store para não ver actions do Redux-Persist

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,16 +1,45 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-import { persistStore } from 'redux-persist';
+import {
+    persistStore,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
 import persistedReducer from '../config/persistConfig';
 import rootSaga from '../sagas/rootSaga';
 
+// Constantes renomeadas
+const ignoredPersistActions = [
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+];
+
+// Configuração do middleware do Redux Saga
 const sagaMiddleware = createSagaMiddleware();
+
+// Configuração do store Redux
 const store = configureStore({
     reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(sagaMiddleware),
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ignoredPersistActions,
+            },
+        }).concat(sagaMiddleware),
 });
+
+// Configuração do persistor
 const persistor = persistStore(store);
+
+// Execução do saga middleware
 sagaMiddleware.run(rootSaga);
 
 export { store, persistor };
